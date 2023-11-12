7b2728f584056226fc10a9f89d4913b722af18ab
Angelo Ilardi <ange448@gmail.com>
2023-11-12 21:17:08 +0000

    completed mobile version

--- app/page.tsx
+++ app/page.tsx
@@ -5,25 +5,43 @@ const icons = ["icon-document.svg", "icon-folder.svg", "icon-upload.svg"];
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center bg-[url(/images/bg-mobile.png)] justify-center bg-cover bg-no-repeat gap-6">
-      <div className="flex flex-col bg-delft_blue w-[300px] h-[180px] rounded-b-lg rounded-tl-lg rounded-tr-[100px] pl-5 justify-evenly">
-        <div className="p-3">
+      <div className="flex flex-col bg-delft_blue w-[330px] h-[200px] rounded-b-lg rounded-tl-lg rounded-tr-[100px] px-7 justify-evenly">
           <Image src="/images/logo.svg" alt="logo" width={135} height={40} />
-        </div>
-        <div className="flex gap-3">
+        <div className="flex gap-5">
           {icons.map((icon: string, i: number) => {
             return (
               <div
                 key={i}
                 className="bg-oxford_blue p-3 rounded-lg flex items-center"
               >
-                <Image src={`/images/${icon}`} width={24} height={16} alt="" />
+                <Image src={`/images/${icon}`} width={24} height={24} alt="" />
               </div>
             );
           })}
         </div>
       </div>
-      <div className="flex flex-col bg-delft_blue w-[300px] h-[150px] rounded-lg pl-5 justify-evenly">
-        <p>You’ve used 815 GB of your storage</p>
+      <div className="flex flex-col bg-delft_blue w-[330px] h-[180px] rounded-lg px-5 relative gap-3">
+        <p className="text-sm text-center mt-10 text-delft_blue-900">
+          You’ve used 815 GB of your storage
+        </p>
+        <div className="rounded-full w-full h-5 bg-delft_blue-400 p-1">
+          <div
+            className={`flex bg-gradient-to-r from-[hsl(6,100%,80%)] h-full to-[hsl(335,100%,65%)] rounded-full w-[${
+              (815 / 1000) * 100
+            }%] p-1 items-center relative`}
+          >
+            <div className="absolute bg-white w-2.5 h-2.5 rounded-full right-0.5"></div>
+          </div>
+        </div>
+        <div className="flex justify-between text-xs font-semibold text-delft_blue-900">
+          <p>0 GB</p>
+          <p>1000 GB</p>
+        </div>
+        <div className="w-[150px] h-[60px] bg-white flex justify-center rounded-md items-center absolute -bottom-[20%] left-[25%]">
+          <p className="text-xs text-taupe_gray font-bold">
+            <span className="text-4xl font-bold text-black">185 </span>GB LEFT
+          </p>
+        </div>
       </div>
     </main>
   );
